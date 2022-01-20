5479cc4e42cb5c1ecde3c52f163b34d1595bb23f
daipom <fukuda@clear-code.com>
2022-01-20 13:05:37 +0900

    Revert commits about adding touch event to waylandwindow
    Because we want to use upstream fix instead.
    
    This reverts the following commits:
    
    - 77bccd1675b729f8d722f71396a97c4b25163b27
    - fee3123dc9f08195b9d1f278574d97a1a8153fd4
    - f37402e4510ef871a8d898688bc6463b2b220e7f

--- src/ui/classic/waylandinputwindow.cpp
+++ src/ui/classic/waylandinputwindow.cpp
@@ -48,14 +48,6 @@ WaylandInputWindow::WaylandInputWindow(WaylandUI *ui)
             repaint();
         }
     });
-    window_->touchDown().connect([this](int x, int y) {
-        click(x, y, false);
-        repaint();
-    });
-    window_->touchUp().connect([this](int x, int y) {
-        click(x, y, true);
-        repaint();
-    });
     window_->axis().connect([this](int, int, uint32_t axis, wl_fixed_t value) {
         if (axis != WL_POINTER_AXIS_VERTICAL_SCROLL) {
             return;
--- src/ui/classic/waylandpointer.cpp
+++ src/ui/classic/waylandpointer.cpp
@@ -19,13 +19,6 @@ WaylandPointer::WaylandPointer(wayland::WlSeat *seat) {
         } else if (!(caps & WL_SEAT_CAPABILITY_POINTER) && pointer_) {
             pointer_.reset();
         }
-
-        if ((caps & WL_SEAT_CAPABILITY_TOUCH) && !touch_) {
-            touch_.reset(seat->getTouch());
-            initTouch();
-        } else if (!(caps & WL_SEAT_CAPABILITY_POINTER) && touch_) {
-            touch_.reset();
-        }
     });
 }
 
@@ -67,28 +60,6 @@ void WaylandPointer::initPointer() {
             window->axis()(focusX_, focusY_, axis, value);
         }
     });
-}
-
-void WaylandPointer::initTouch() {
-    touch_->down().connect(
-        [this](uint32_t, uint32_t, wayland::WlSurface *surface, int, wl_fixed_t sx, wl_fixed_t sy) {
-            auto *window = static_cast<WaylandWindow *>(surface->userData());
-            if (!window) {
-                return;
-            }
-            focus_ = window->watch();
-            focusX_ = wl_fixed_to_int(sx);
-            focusY_ = wl_fixed_to_int(sy);
-            window->touchDown()(focusX_, focusY_);
-        });
-    touch_->up().connect(
-        [this](uint32_t, uint32_t, int) {
-            if (auto *window = focus_.get()) {
-                window->touchUp()(focusX_, focusY_);
-                focus_.unwatch();
-                window->leave()();
-            }
-        });
 }
 
 } // namespace fcitx::classicui
